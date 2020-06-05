785adde125cef1980535bb182263d6c7fa3c2fe8
shenqiangbin <1348907384@qq.com>
2020-06-05 14:47:16 +0800

    补充C#调用

---
+++ InvokeTest/InvokeTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace InvokeTest
+{
+    class Program
+    {
+        [DllImport("dlltest.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Auto)]
+        static extern IntPtr goStart(String StrConn, String strSimuationId, String StrJson);
+
+        static void Main(string[] args)
+        {
+            IntPtr pRet = goStart("StrConn", "strSimuationId", "pStrJson");
+            int nRet = pRet.ToInt32();
+        }
+
+
+    }
+}
